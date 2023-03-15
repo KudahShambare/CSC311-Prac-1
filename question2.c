cc624257c11a66c431dfa81078495b0ca3081da5
KudahShambare <kudahshambare263@gmail.com>
2023-03-16 00:35:09 +0200

    Q2 done

--- question2.c
+++ question2.c
@@ -14,8 +14,6 @@ void main() {
  char* letterPointer = &letter;
  
 printf("\nThe address of int integer is %p\n",&integer);
-printf("\nThe address of int integer is %p\n",intPointer);
-
 printf("The address of double decimal is %p\n",&decimal);
 printf("The address of char letter is %p\n",&letter);
 printf("The address of int* intPointer %p\n",&intPointer);
@@ -26,13 +24,19 @@ printf("The address of char* letterPointer is %p\n",&letterPointer);
  
 printf("\nThe value of int integer is %d\n",integer);
 printf("The value of double decimal is %f\n",decimal);
-printf("The value of char letter is %s\n",letter);
+printf("The value of char letter is %c\n",letter);
+printf("The value of int* intPointer %p\n",intPointer);
+printf("The value of double* doublePointer is %p\n",doublePointer);
+printf("The value of char* letterPointer is %p\n",letterPointer);
 
-printf("The value of int* intPointer %d\n",*intPointer);
-printf("The value of double* doublePointer is %f\n",*doublePointer);
-printf("The value of char* letterPointer is %s\n",letterPointer);
 
- 
+printf("\nThe size of int integer is %ld bytes \n", sizeof(integer));
+printf("The size of double decimal is %ld bytes \n", sizeof(decimal));
+printf("The size of char letter is %ld bytes \n", sizeof(letter));
+printf("The size of int* intPointer %ld bytes \n", sizeof(intPointer));
+printf("The size of double* doublePointer is %ld bytes \n", sizeof(doublePointer));
+printf("The size of char* letterPointer is %ld bytes \n", sizeof(letterPointer));
+
  
 
     
